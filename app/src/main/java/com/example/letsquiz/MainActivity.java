78acb8681fea31988a3eaf9253d9faefacd16acc
Harsh <harsh.sharma2021@gmail.com>
2021-10-27 17:12:02 +0530

    First Commit

---
+++ app/src/main/java/com/example/letsquiz/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.letsquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+
+    }
+}
