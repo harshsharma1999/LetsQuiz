3fabe7088ae5ffdc03c243faa46859f9c9403edf
Harsh <harsh.sharma2021@vitbhopal.ac.in>
2021-10-28 13:56:35 +0530

    login desgin implemented

--- app/src/main/java/com/example/letsquiz/MainActivity.java
+++ app/src/main/java/com/example/letsquiz/MainActivity.java
@@ -1,23 +1,72 @@
 package com.example.letsquiz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    TextInputLayout t1,t2;
+    ProgressBar bar;
 
-    private FirebaseAnalytics mFirebaseAnalytics;
+    private Object TextInputLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
+t1=(TextInputLayout)findViewById(R.id.Email);
+        t2=(TextInputLayout)findViewById(R.id.pwd);
+        bar=(ProgressBar)findViewById(R.id.progressBar);
+
+
+
+    }
+
+    public void signuphere(View view) {
+bar.setVisibility(View.VISIBLE);
+        String Email = Objects.requireNonNull(t1.getEditText()).getText().toString();
+        String Password= Objects.requireNonNull(t2.getEditText()).getText().toString();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(Email, Password)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                        bar.setVisibility(View.INVISIBLE);
+                        t1.getEditText().setText("");
+                            t2.getEditText().setText("");
+                            Toast.makeText(getApplicationContext(),"Registered Successfully",Toast.LENGTH_LONG).show();
+                        } else {
+                            bar.setVisibility(View.INVISIBLE);
+                            t1.getEditText().setText("");
+                            t2.getEditText().setText("");
+                            Toast.makeText(getApplicationContext(),"Process Error",Toast.LENGTH_LONG).show();
+
+                        }
+
+                        // ...
+                    }
+                });
 
     }
 }
