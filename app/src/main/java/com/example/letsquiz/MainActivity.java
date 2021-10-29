c891366483f809cd843a5d34b123894165f00f07
mayank <chaudharysinghmayank00gmail.com>
2021-10-29 16:27:41 +0530

    unwanted code has been removed

--- app/src/main/java/com/example/letsquiz/MainActivity.java
+++ app/src/main/java/com/example/letsquiz/MainActivity.java
@@ -20,7 +20,7 @@ import com.google.firebase.auth.FirebaseUser;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    TextInputLayout t1,t2;
+    TextInputLayout t1, t2;
     ProgressBar bar;
 
     private Object TextInputLayout;
@@ -31,44 +31,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        // Obtain the FirebaseAnalytics instance.
-        FirebaseAnalytics mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-
-t1=(TextInputLayout)findViewById(R.id.Email);
-        t2=(TextInputLayout)findViewById(R.id.pwd);
-        bar=(ProgressBar)findViewById(R.id.progressBar);
-
+        t1 = (TextInputLayout) findViewById(R.id.Email);
+        t2 = (TextInputLayout) findViewById(R.id.pwd);
+        bar = (ProgressBar) findViewById(R.id.progressBar);
 
 
     }
 
-    public void signuphere(View view) {
-bar.setVisibility(View.VISIBLE);
-        String Email = Objects.requireNonNull(t1.getEditText()).getText().toString();
-        String Password= Objects.requireNonNull(t2.getEditText()).getText().toString();
-        mAuth = FirebaseAuth.getInstance();
-        mAuth.createUserWithEmailAndPassword(Email, Password)
-                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                        bar.setVisibility(View.INVISIBLE);
-                        t1.getEditText().setText("");
-                            t2.getEditText().setText("");
-                            Toast.makeText(getApplicationContext(),"Registered Successfully",Toast.LENGTH_LONG).show();
-                        } else {
-                            bar.setVisibility(View.INVISIBLE);
-                            t1.getEditText().setText("");
-                            t2.getEditText().setText("");
-                            Toast.makeText(getApplicationContext(),"Process Error",Toast.LENGTH_LONG).show();
 
-                        }
-
-                        // ...
-                    }
-                });
-
-    }
 }
